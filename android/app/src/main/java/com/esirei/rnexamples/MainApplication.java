4297e3c4638c09a392f181452d1a5ae9e33279f0
Esi're <esiri92@gmail.com>
2019-09-30 00:56:11 +0100

    Renamed App.

--- android/app/src/main/java/com/esirei/rnexamples/MainApplication.java
+++ android/app/src/main/java/com/esirei/rnexamples/MainApplication.java
@@ -1,4 +1,4 @@
-package com.rntemplate;
+package com.esirei.rnexamples;
 
 import android.app.Application;
 import android.content.Context;
--- ios/RNExamplesTests/RNExamplesTests.m
+++ ios/RNExamplesTests/RNExamplesTests.m
@@ -14,11 +14,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface rnTemplateTests : XCTestCase
+@interface RNExamplesTests : XCTestCase
 
 @end
 
-@implementation rnTemplateTests
+@implementation RNExamplesTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
